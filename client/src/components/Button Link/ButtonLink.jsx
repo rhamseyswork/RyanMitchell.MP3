03348296266790cb85fee6014824150e29b36d57
Rhamseys <rhamseyswork@gmail.com>
2024-07-03 06:34:35 -0700

    build

--- client/src/components/Button Link/ButtonLink.jsx
+++ client/src/components/Button Link/ButtonLink.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaSpotify } from 'react-icons/fa'
+
 
 const ButtonLink = ({ target="_blank", rel="noopener noreferrer", alt, to, img, children}) => {
   return (
     <button>
         <Link target={target} rel={rel} to={to}>
           <img src={img} alt={alt} />
+          {img}
+          <FaSpotify />
           {children}
         </Link>
     </button>
