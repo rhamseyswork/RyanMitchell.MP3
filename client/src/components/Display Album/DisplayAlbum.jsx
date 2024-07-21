2c1cc3a174b6079b158747c5e4effb74d7395b78
Rhamseys <rhamseyswork@gmail.com>
2024-07-21 10:58:14 -0700

    display Album update

--- client/src/components/Display Album/DisplayAlbum.jsx
+++ client/src/components/Display Album/DisplayAlbum.jsx
@@ -3,28 +3,27 @@ import styles from './DisplayAlbum.module.css'
 
 function DisplayAlbum() {
   return (
-    <div>
-      <h2>Destory Of the World (Out Now)</h2>
-      <div className={styles.container}>
+    <diV className={styles.container}>
+      <div className={styles.albumContainer}>
         <img
           className={styles.img}
           src="/Hero.webp"
           alt="Hero Image"
-          width="200px"
-          height="200px"
+          width="150px"
+          height="150px"
           style={{ objectFit: 'cover' }}
-          />
-          <h5>Destory Of the World</h5>
-          <div className={styles}>
-            <p>Stream Now On</p>
-            <ul>
-              <li>1</li>
-              <li>2</li>
-              <li>3</li>
-            </ul>
-          </div>
+        />
+        <div className={styles.text}>
+          <h2 className={styles.title}>Destory Of the World (Out Now)</h2>
+          <h5 className={styles.albumName}>Destory Of the World</h5>
+        </div>
       </div>
-    </div>
+        <ul className={styles.btn}>
+          <li>1</li>
+          <li>2</li>
+          <li>3</li>
+        </ul>
+    </diV>
   )
 }
 
